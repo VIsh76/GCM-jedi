Graph is now complete, fix exploding gradient

src/forecaster.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 from .dycore import Dycore
 from .physic import Physics, Normalizer
-from .utils import Forcing_Generator
 
 class Forecaster(nn.Module):
     def __init__(self, dycore:Dycore, 
@@ -41,6 +40,6 @@ class Forecaster(nn.Module):
         # Process:
         surface, column = self.physic(var_surface, var_column, forced)
         # UnNorm:
-        column = self.input_norm['column'].unnorm(var_column)
-        surface = self.input_norm['surface'].unnorm(var_surface)
+        column = self.input_norm['column'].unnorm(column)
+        surface = self.input_norm['surface'].unnorm(surface)
         return  surface, column

src/physic/blocks.py
@@ -14,10 +14,10 @@ class MLPBlock(nn.Module):
         else:
             return x
 
-class ConvBlock(nn.Module):
+class Conv1DBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, activation:bool) -> None:
         super().__init__()
-        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, padding='same')
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(kernel_size, 1), padding='same')
         self.activation = activation
         self.act = nn.SELU()
  

src/physic/embedding.py
@@ -1,5 +1,6 @@
 from torch import nn
-from.blocks import ConvBlock, MLPBlock
+from.blocks import Conv1DBlock, MLPBlock
+import torch
 
 class NoEmbedding(nn.Module):
     def __init__(self):
@@ -16,18 +17,34 @@ class Surface_Embedding(nn.Module):
         for _ in range(n_blocks-2):
             self.blocks.append(  MLPBlock(hidden_size, hidden_size, activation=True) )
         self.blocks.append(  MLPBlock(hidden_size, output_size, activation=False) )
-        
-
-class Vertical_Embedding(nn.Module):
-    def __init__(self, n_blocks, input_size, hidden_size, output_size, kernel_size):
-        super(Vertical_Embedding, self).__init__()
-        self.blocks = []
-        self.blocks.append( ConvBlock(input_size, hidden_size, kernel_size, activation=True) )
-        for _ in range(n_blocks-2):
-            self.blocks.append(  ConvBlock(hidden_size, hidden_size, kernel_size, activation=True) )
-        self.blocks.append(  ConvBlock(hidden_size, output_size, kernel_size, activation=False) )
+        for i in range(len(self.blocks)):
+            self.add_module(f"sur_embd_block_{i}", self.blocks[i])
 
     def forward(self, x):
         for i in range(len(self.blocks)):
             x = self.blocks[i](x)
+        return x
+     
+
+class Column_Embedding(nn.Module):
+    def __init__(self, n_blocks, input_size, hidden_size, output_size, kernel_size):
+        super(Column_Embedding, self).__init__()
+        self.blocks = []
+        self.blocks.append( Conv1DBlock(input_size, hidden_size, kernel_size, activation=True) )
+        for _ in range(n_blocks-2):
+            self.blocks.append(  Conv1DBlock(hidden_size, hidden_size, kernel_size, activation=True) )
+        self.blocks.append(  Conv1DBlock(hidden_size, output_size, kernel_size, activation=False) )
+        for i in range(len(self.blocks)):
+            self.add_module(f"col_embd_block_{i}", self.blocks[i])
+
+
+    def forward(self, x):
+        # Input size is (bs, horizontal, lev, nb_vars) -> Reshape
+        # Pytorch uses convolution differently where the variable dimension is not not at the end but at the start!
+        x = torch.swapaxes(x, -1, 1) # Swich (bs, profile_points, lev, vars) to  (bs, vars, lev, profile_points)
+        # Perform the kernels
+        for i in range(len(self.blocks)):
+            x = self.blocks[i](x)
+        # Put the dimension at the end
+        x = torch.swapaxes(x, -1, 1) # Swich(bs, vars, lev, profile_points) to (bs, profile_points, lev, vars) 
         return x

src/physic/process.py
@@ -11,7 +11,11 @@ class Process(nn.Module):
             self.layers.append(nn.Linear(hidden_dims, hidden_dims))
             self.act.append(nn.SiLU())
         self.layers.append(nn.Linear(hidden_dims, output_dims))   
-        # Last layer linear
+
+        for i in range(len(self.layers)):
+            self.add_module(f"process_lin_{i}", self.layers[i])
+        for j in range(len(self.act)):
+            self.add_module(f"process_act_{j}", self.act[j])
 
     def forward(self, x):
         for i in range(len(self.layers)):