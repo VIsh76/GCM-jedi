b41498d46a408115c422bf12c71e724514f60c25
Ubuntu <ubuntu@ip-172-31-16-123.ec2.internal>
2024-02-05 18:46:56 +0000

    Working version 0 with learning

--- src/model/physic/embedding.py
+++ src/model/physic/embedding.py
@@ -27,13 +27,13 @@ class Surface_Embedding(nn.Module):
      
 
 class Column_Embedding(nn.Module):
-    def __init__(self, n_blocks, input_size, hidden_size, output_size, kernel_size, kernel_dim:bool, **kwarg):
+    def __init__(self, n_blocks, input_size, hidden_size, output_size, kernel_size, **kwarg):
         super(Column_Embedding, self).__init__()
         self.blocks = []
-        self.blocks.append( ConvBlock(input_size, hidden_size, kernel_size, activation=True, kernel_dim=kernel_dim) )
+        self.blocks.append( ConvBlock(input_size, hidden_size, kernel_size, activation=True) )
         for _ in range(n_blocks-2):
-            self.blocks.append(  ConvBlock(hidden_size, hidden_size, kernel_size, activation=True, kernel_dim=kernel_dim) )
-        self.blocks.append(  ConvBlock(hidden_size, output_size, kernel_size, activation=False, kernel_dim=kernel_dim) )
+            self.blocks.append(  ConvBlock(hidden_size, hidden_size, kernel_size, activation=True) )
+        self.blocks.append(  ConvBlock(hidden_size, output_size, kernel_size, activation=False) )
         for i in range(len(self.blocks)):
             self.add_module(f"col_embd_block_{i}", self.blocks[i])
 
